5ce93004be1c99d22b97f2593bb03e1cd73d44aa
23643studentdorset <23643@student.dorset-college.ie>
2022-04-17 17:30:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.onlineshopping_mobileapp_23643_lucianogimenez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
