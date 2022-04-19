aab1aab9514dbaa9f0fcdf5f20dc8c192954e982
23643studentdorset <23643@student.dorset-college.ie>
2022-04-19 15:31:40 +0100

    grabbing 1 user data

--- app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/MainActivity.kt
+++ app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/MainActivity.kt
@@ -2,10 +2,58 @@ package com.example.onlineshopping_mobileapp_23643_lucianogimenez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.UserData
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.login_login_button)
+        buttonClick.setOnClickListener {
+            if (findViewById<EditText>(R.id.user_name_login).text.toString() != "" &&
+                findViewById<EditText>(R.id.password_login).text.toString() != ""
+            ) {
+                val username = findViewById<EditText>(R.id.user_name_login).text.toString()
+                val password = findViewById<EditText>(R.id.password_login).text.toString()
+                fetchJsonData(username!!, password!!)
+            } else {
+                Toast.makeText(
+                    this,
+                    "You have to write a username and/or password",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    }
+
+    private fun fetchJsonData(username: String, password: String) {
+        val url = "https://fakestoreapi.com/users/1"
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+                //println (body)
+                val gson = GsonBuilder().create()
+                val userData = gson.fromJson(body, User::class.java)
+                println(userData)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.i("lucho", "$e")
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/User.kt
@@ -0,0 +1,11 @@
+package com.example.onlineshopping_mobileapp_23643_lucianogimenez
+
+data class User( val email:String, val username: String, val password: String,
+                 val name: Name, val address: Address, val phone: String )
+
+data class Address( val city: String, val street: String, val number: Int,
+                   val zipcode: String, val geolocation: Geolocation)
+
+data class Geolocation ( val lat: String, val long: String)
+
+data class Name( val firstname: String, val lastname: String)
