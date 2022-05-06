af37228e7e576132c4e11bed03039d4b93fd01ec
23643studentdorset <23643@student.dorset-college.ie>
2022-05-06 16:28:02 +0100

    click listeners functioning

--- app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/AdapterProductsCart.kt
+++ app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/AdapterProductsCart.kt
@@ -1,9 +1,11 @@
 package com.example.onlineshopping_mobileapp_23643_lucianogimenez
 
+import android.sax.TextElementListener
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -12,10 +14,20 @@ import com.squareup.picasso.Picasso
 
 class AdapterProductsCart (private val productList: ArrayList<Product>, private val quantityList: ArrayList<Int> ): RecyclerView.Adapter<CustomViewHolderCart>() {
 
+    private lateinit var mListener: OnItemClickListener
+
+    interface OnItemClickListener {
+        fun onItemClick(position: Int, operation : Int)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener){
+        mListener = listener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolderCart {
         val layoutInflater = LayoutInflater.from(parent.context)
         val cellForRow = layoutInflater.inflate(R.layout.cart_item, parent, false)
-        return CustomViewHolderCart(cellForRow)
+        return CustomViewHolderCart(cellForRow, mListener)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolderCart, position: Int) {
@@ -36,13 +48,22 @@ class AdapterProductsCart (private val productList: ArrayList<Product>, private 
         return productList.size
     }
 }
-class CustomViewHolderCart(private val view: View):RecyclerView.ViewHolder(view){
+class CustomViewHolderCart(private val view: View, listener: AdapterProductsCart.OnItemClickListener):RecyclerView.ViewHolder(view){
     val title: TextView = itemView.findViewById(R.id.title_cart_item)
     val description: TextView = itemView.findViewById(R.id.description_cart_item)
     val image: ImageView = itemView.findViewById(R.id.product_image_cart_image)
     val rate: TextView = itemView.findViewById(R.id.rate_cart_item)
     val price: TextView = itemView.findViewById(R.id.price_cart_item)
     val quantity: TextView = itemView.findViewById(R.id.quantity)
-    //val minusButton: Button = itemView.findViewById(R.id.button_minus)
-    //val plusButton: Button = itemView.findViewById(R.id.button_plus)
+    val minusButton: ImageButton = itemView.findViewById(R.id.button_minus)
+    val plusButton: ImageButton = itemView.findViewById(R.id.button_plus)
+
+    init{
+        minusButton.setOnClickListener {
+            listener.onItemClick(adapterPosition, 1)
+        }
+        plusButton.setOnClickListener {
+            listener.onItemClick(adapterPosition, 2)
+        }
+    }
 }
--- app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/CartActivity.kt
+++ app/src/main/java/com/example/onlineshopping_mobileapp_23643_lucianogimenez/CartActivity.kt
@@ -1,7 +1,9 @@
 package com.example.onlineshopping_mobileapp_23643_lucianogimenez
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -9,11 +11,13 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.GsonBuilder
 import okhttp3.*
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.IOException
 
 class CartActivity: AppCompatActivity() {
@@ -22,6 +26,7 @@ class CartActivity: AppCompatActivity() {
     private lateinit var quantityList: ArrayList<Int>
     private lateinit var productList: ArrayList<Product>
 
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cart)
@@ -29,7 +34,6 @@ class CartActivity: AppCompatActivity() {
         val sharedPreferences = getSharedPreferences(MY_APP_PREFERENCES, Context.MODE_PRIVATE)!!
         val sharedToken = sharedPreferences.getString("Token", "No Login")
         val sharedId = sharedPreferences.getInt("id", -1)
-        //fetchProducts()
         var jsonCurrentCart = sharedPreferences.getString(CURRENT_CART_KEY, null)
 
 
@@ -37,6 +41,13 @@ class CartActivity: AppCompatActivity() {
         recyclerViewCart.layoutManager = LinearLayoutManager(this)
 
         fetchACartOfFrom(sharedId, jsonCurrentCart)
+
+        findViewById<Button>(R.id.button_checkout).setOnClickListener {
+            if (jsonCurrentCart != null){
+                postACart(jsonCurrentCart,sharedToken!!, sharedPreferences)
+            }
+
+        }
 
         findViewById<Button>(R.id.shop_button_cart).setOnClickListener {
             val intent = Intent(this, ShopActivity::class.java)
@@ -98,86 +109,61 @@ class CartActivity: AppCompatActivity() {
                 }
 
                 Handler(Looper.getMainLooper()).postDelayed({
-                    recyclerViewCart.adapter = AdapterProductsCart(productList, quantityList)
+                    var adapter = AdapterProductsCart(productList, quantityList)
+                    recyclerViewCart.adapter = adapter
+                    adapter.setOnItemClickListener(object : AdapterProductsCart.OnItemClickListener{
+                        override fun onItemClick(position: Int, operation: Int ) {
+                            println("position:$position operation:$operation")
+                            if (operation == 1){
+                                quantityList[position] -= 1
+                                println("quantity:${quantityList[position]}")
+                            }else{
+                                quantityList[position] += 1
+                                println("quantity:${quantityList[position]}")
+                            }
+
+                        }
+
+                    })
+
                     findViewById<TextView>(R.id.loading).visibility = View.GONE
                     recyclerViewCart.visibility = View.VISIBLE}, 3000)
-                /*
-                runOnUiThread{
-                    //println("productList2: $productList")
-                    //println("quantityList2: $quantityList")
-                    while (productList.size == 0) {
-                        recyclerViewCart.adapter = AdapterProductsCart(productList, quantityList)
-                    }
-                }
+            }
+        })
+    }
 
-                 */
+    private fun postACart(cart: String, token: String, sharedPreferences : SharedPreferences){
+        val urlPostCart = "https://fakestoreapi.com/carts"
+        val client = OkHttpClient()
+        val requestPostCart = Request
+            .Builder()
+            .url(urlPostCart)
+            .addHeader("Content-Type", "application/json; charset=utf-8")
+            .addHeader("Authorization","Bearer $token")
+            .post(cart.toRequestBody())
+            .build()
+        client.newCall(requestPostCart).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                Log.i("lucho", "$e")
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.code == 200){
+                    sharedPreferences.edit().remove(CURRENT_CART_KEY).apply()
+                    runOnUiThread{
+                        Toast.makeText(this@CartActivity,"Order Sent", Toast.LENGTH_LONG).show()
+                        recyclerViewCart.visibility = View.GONE
+                        quantityList.clear()
+                        productList.clear()
+                    }
+
+
+                }
             }
         })
     }
 }
 
-
-    /*
-    private fun fetchProducts(){
-        quantityList = arrayListOf()
-        productList = arrayListOf()
-
-        val url = "https://fakestoreapi.com/carts/5"
-        val request = Request.Builder().url(url).build()
-        val client = OkHttpClient()
-        client.run {
-            newCall(request).enqueue(object :Callback{
-                override fun onFailure(call: Call, e: IOException) {
-                    Log.i("lucho", "$e")
-                }
-
-                override fun onResponse(call: Call, response: Response) {
-                    if (response.isSuccessful){
-                        val body = response.body?.string()
-                       // println("cart: $body")
-                        var url2: String
-                        val gson = GsonBuilder().create()
-                        val cart = gson.fromJson(body, Cart::class.java)
-                        for (item in cart.products){
-                            //println(item.quantity)
-                            //println(item.productId)
-                            quantityList.add(item.quantity)
-                            //println("quantityList: $quantityList")
-                            url2 = "https://fakestoreapi.com/products/${item.productId}"
-                            val requestProduct = Request.Builder().url(url2).build()
-                            client.run{
-                                newCall(requestProduct).enqueue(object :Callback{
-                                    override fun onFailure(call: Call, e: IOException) {
-                                        Log.i("lucho", "$e")
-                                    }
-
-                                    override fun onResponse(call: Call, response: Response) {
-                                        val body2 = response.body?.string()
-                                        //println("body:$body2")
-                                        val gson2 = GsonBuilder().create()
-                                        val product = gson2.fromJson(body2, Product::class.java)
-                                        //println("product: $product")
-                                        productList.add(product)
-                                        //println("productList: $productList")
-                                    }
-                                })
-                            }
-                        }
-
-                    }
-                    runOnUiThread {
-                        //println("productList2: $productList")
-                        //println("quantityList2: $quantityList")
-                        while (productList.size == 0){
-                            recyclerViewCart.adapter = AdapterProductsCart(productList, quantityList)
-                        }
-                    }
-                }
-            })
-        }
-    }
-}
-*/
 
 
 
